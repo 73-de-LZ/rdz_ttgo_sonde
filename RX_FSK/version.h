17fa6a05b448ea84a76ea136cebe0bd10dd8a964
73-de-LZ <suser@mail.bg>
2025-02-12 21:21:53 +0200

    Update version.h

--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,6 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "dev20250210";
+const char *version_id = "devLZ20250211";
 const int FS_MAJOR=3;
 const int FS_MINOR=3;
+
+// LZ4TU-5 MODs based on devel20250210 rs41 X
